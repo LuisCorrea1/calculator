4db836f88544b64c747e8bee466b94333fc57fcd
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-29 21:26:24 -0600

    Updating functions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
+class Calculator {
+    constructor(input,output) {
+        this.input = input;
+        this.output = output;
+    }
+
+    clear(){
+
+    }
+
+    delete(){
+
+    }
+
+    appendNumber(number){
+
+    }
+
+    selectOperation(operation){
+
+    }
+
+    makeOperation(){
+
+    }
+
+    updateDisplay(){
+        
+    }
+}
+
 const numberBtn = document.querySelectorAll('[data-number]');
 const operationBtn = document.querySelectorAll('[data-operation]');
 const equalBtn = document.querySelector('[data-equal]');
@@ -5,3 +36,4 @@ const clearBtn = document.querySelector('[data-clear]');
 const deleteBtn = document.querySelector('[data-delete]');
 const input = document.querySelector('[data-operand1]');
 const output = document.querySelector('[data-operand2]');
+
