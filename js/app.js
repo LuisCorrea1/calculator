0c6bcc43e18506eca16042552aa153e257cea99e
LuisCorrea1 <launchx02938@innovaccion.mx>
2023-03-29 20:17:55 -0600

    Creating variables for all the data

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const numberBtn = document.querySelectorAll('[data-number]');
+const operationBtn = document.querySelectorAll('[data-operation]');
+const equalBtn = document.querySelector('[data-equal]');
+const clearBtn = document.querySelector('[data-clear]');
+const deleteBtn = document.querySelector('[data-delete]');
+const input = document.querySelector('[data-operand1]');
+const output = document.querySelector('[data-operand2]');
